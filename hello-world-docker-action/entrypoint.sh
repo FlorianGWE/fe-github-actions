7af5c91615cca5e8b34ebc708b830573425ca06e
Florian <florian.g.w.e4@gmail.com>
2024-02-28 12:32:55 +0100

    Update entrypoint.sh get user [no ci]

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -3,5 +3,8 @@
 echo "hello $1 you run my Docker action"
 echo "Time $(date)"
 
+echo "User $USER"
+echo "Whoami $(whoami)"
+
 echo "time=$(date)" >> $GITHUB_OUTPUT
 echo "Runner=$(whoami)" >> $GITHUB_OUTPUT
